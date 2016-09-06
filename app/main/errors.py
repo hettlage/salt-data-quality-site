a62660a75d05a7a0ed1690e26fc95e7aaeea3027
Christian Hettlage <hettlage@saao.ac.za>
2016-09-07 00:42:33 +0200

    Log stack trace for errors, return error code

--- app/main/errors.py
+++ app/main/errors.py
@@ -5,16 +5,16 @@ from . import main
 
 @main.errorhandler(500)
 def internal_server_error(e):
-    current_app.logger.error(str(e))
-    return 'Replace me with proper error handling.'
+    current_app.logger.error(str(e), exc_info=1)
+    return render_template('500.html'), 500
 
 
 @main.errorhandler(404)
 def file_not_found_error(e):
-    return render_template('404.html')
+    return render_template('404.html'), 404
 
 
 @main.errorhandler(Exception)
 def exception_raised(e):
-    current_app.logger.error(str(e))
-    return 'Replace me with proper error handling.'
+    current_app.logger.error(str(e), exc_info=1)
+    return render_template('500.html'), 500
