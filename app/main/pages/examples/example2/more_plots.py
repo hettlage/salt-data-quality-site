7d6755632f5203aebb89c3d197708b90bdd47ba7
Christian Hettlage <hettlage@saao.ac.za>
2016-09-07 21:17:59 +0200

    Example plots using Bokeh

--- app/main/pages/examples/example2/more_plots.py
+++ app/main/pages/examples/example2/more_plots.py
@@ -1,13 +1,31 @@
+import numpy as np
+
+from bokeh.embed import components
+from bokeh.plotting import figure
+
 from app.main.data_quality import data_quality
 
 
-@data_quality(name='Second Plot', caption='Another shiny plot.')
-def plot1(from_date, to_date, include_errors):
-    return '<div>This is plot 2 (from: {from_date}, to: {to_date}, include errors: {include_errors})</div>' \
-        .format(from_date=from_date, to_date=to_date, include_errors=include_errors)
+@data_quality(name='sine', caption='The sine function.')
+def sine_plot(from_date, to_date, include_errors):
+    x = np.linspace(-10, 10, 100)
+    y = np.sin(x)
+    p = figure(title='Sine', x_axis_label='x', y_axis_label='y')
+    p.line(x, y)
+
+    script, div = components(p)
+
+    return '<div>{script}{div}</div>'.format(script=script, div=div)
+
 
 
 @data_quality(name='Third Plot', caption='Yet another shiny plot.')
 def plot2(from_date, to_date, include_errors):
-    return '<div>This is plot 2 (from: {from_date}, to: {to_date}, include errors: {include_errors})</div>' \
-        .format(from_date=from_date, to_date=to_date, include_errors=include_errors)
+    x = np.linspace(-10, 10, 100)
+    y = np.cos(x)
+
+    p = figure(title='Cosine', x_axis_label='x', y_axis_label='y')
+    p.line(x, y)
+    script, div = components(p)
+
+    return '<div>{script}{div}</div>'.format(script=script, div=div)
