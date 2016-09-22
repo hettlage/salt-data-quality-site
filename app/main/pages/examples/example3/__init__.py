2f5cae8ee3a547cdcb352ff59456c5e8c07c18a4
Christian Hettlage <hettlage@saao.ac.za>
2016-09-22 13:32:01 +0200

    Improved Bokeh server example

--- app/main/pages/examples/example3/__init__.py
+++ app/main/pages/examples/example3/__init__.py
@@ -1,4 +1,7 @@
+from urllib.parse import urlparse
+
 from bokeh.embed import autoload_server
+from flask import request
 
 
 def title():
@@ -6,6 +9,13 @@ def title():
 
 
 def content():
-    script1 = autoload_server(model=None, app_path='/sine', url='http://localhost:5100')
-    script2 = autoload_server(model=None, app_path='/cosine', url='http://localhost:5100')
+    up = urlparse(request.url)
+    scheme = up.scheme
+    host = up.netloc
+    port = up.port
+    if port:
+        host = host[:-(len(str(port)) + 1)]
+    bokeh_server_url = '{scheme}://{host}:5100'.format(scheme=scheme, host=host)
+    script1 = autoload_server(model=None, app_path='/sine', url=bokeh_server_url)
+    script2 = autoload_server(model=None, app_path='/cosine', url=bokeh_server_url)
     return '<div>' + script1 + '</div>\n<div>' + script2 + '</div>'
