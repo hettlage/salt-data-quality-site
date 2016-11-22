492f765c541fed717480df18497e8b27ab161cd4
Christian Hettlage <hettlage@saao.ac.za>
2016-11-22 14:55:56 +0200

    Explicitly use virtual environment Python

--- run_tests.sh
+++ run_tests.sh
@@ -2,21 +2,21 @@
 
 ERROR=0
 
-python -m unittest discover tests/unittests
+venv/bin/python -m unittest discover tests/unittests
 if [[ $? != 0 ]]
 then
     ERROR=1
     echo 'Unit tests did NOT pass!' >&2
 fi
 
-python -m behave tests/features
+venv/bin/python -m behave tests/features
 if [[ $? != 0 ]]
 then
     ERROR=1
     echo 'Behave tests did NOT pass!' >&2
 fi
 
-python -m pycodestyle --ignore=E402 --max-line-length=120 app tests
+venv/bin/python -m pycodestyle --ignore=E402 --max-line-length=120 app tests
 if [[ $? != 0 ]]
 then
     ERROR=1
